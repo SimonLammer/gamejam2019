13befa6f3e88acff2bb9cab1f39154ee6ad661f3
Simon Lammer <lammer.simon@gmail.com>
2019-08-11 15:26:15 +0200

    add icemen to city chunks

--- game/scenes/chunks/city/city1.gd
+++ game/scenes/chunks/city/city1.gd
@@ -1,1 +1,4 @@
 tool extends City
+
+func _ready():
+	$NPCSystem.create_npc("guy", global_position + $Background.map_to_world(Vector2(10, 8), false))
--- game/scenes/chunks/city/city2.gd
+++ game/scenes/chunks/city/city2.gd
@@ -1,1 +1,4 @@
 tool extends City
+
+func _ready():
+	$NPCSystem.create_npc("guy", global_position + $Background.map_to_world(Vector2(11, 6), false))
